53a82f8867256e6dd413c3572ee74a6164b47a38
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-12 17:44:16 +0100

    elavation btn

--- components/Button.js
+++ components/Button.js
@@ -21,7 +21,8 @@ const styles = StyleSheet.create({
         borderColor: '#333333',
         backgroundColor: '#333333',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        elevation: 20
     },
     text:{
         color: '#fff',
